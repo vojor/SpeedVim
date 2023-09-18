f54bd55ccf42100da61f4d4f05b06ac781c3d3fe
alounel <uhavous@gmail.com>
2023-09-19 01:37:21 +0800

    add python3_host_prog

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -24,7 +24,7 @@ vim.o.autoread       = true
 vim.opt.confirm      = true
 -- 编码设置和检测
 vim.o.fileencoding   = "utf-8"
-vim.o.fileencodings  = "utf-8,ucs-bom,utf-16,utf-32,gb18030,gbk,gb2312,latin1"
+vim.o.fileencodings  = "ucs-bom,utf-8,utf-16,utf-32,gb18030,gbk,gb2312,latin1"
 -- 显示左侧图标指示列
 vim.wo.signcolumn    = "yes"
 -- 搜索设置
@@ -40,43 +40,45 @@ vim.opt.mouse        = "a"
 -- 启用系统剪切板
 vim.opt.clipboard:append("unnamedplus")
 -- 缓存文件设置
-vim.opt.backup       = false
-vim.opt.writebackup  = false
-vim.opt.swapfile     = false
+vim.opt.backup          = false
+vim.opt.writebackup     = false
+vim.opt.swapfile        = false
 -- 显示特殊字符
-vim.opt.list         = true
+vim.opt.list            = true
 -- 空格占用以.显示
-vim.opt.listchars    = "space:·,tab:··,eol:↴"
+vim.opt.listchars       = "space:·,tab:··,eol:↴"
 -- complete plus
-vim.o.wildmenu       = true
+vim.o.wildmenu          = true
 -- 终端24位色彩支持
-vim.o.termguicolors  = true
+vim.o.termguicolors     = true
 -- 设置缩进
-vim.o.breakindent    = true
-vim.o.autoindent     = true
-vim.o.tabstop        = 4
-vim.o.shiftwidth     = 4
-vim.o.softtabstop    = 4
-vim.o.expandtab      = true
-vim.o.shiftround     = true
+vim.o.breakindent       = true
+vim.o.autoindent        = true
+vim.o.tabstop           = 4
+vim.o.shiftwidth        = 4
+vim.o.softtabstop       = 4
+vim.o.expandtab         = true
+vim.o.shiftround        = true
 -- 插入括号时短暂跳转到另一半括号
-vim.o.showmatch      = true
+vim.o.showmatch         = true
 -- 光标在行首尾时<Left><Right>可以跳到下一行
-vim.o.whichwrap      = "<,>,[,]"
+vim.o.whichwrap         = "<,>,[,]"
 -- 补全最多显示16行
-vim.o.pumheight      = 16
+vim.o.pumheight         = 16
 -- 使用增强状态栏插件后不再需要 vim 的模式提示
-vim.o.showmode       = false
+vim.o.showmode          = false
 -- redrawtime
-vim.opt.redrawtime   = 10000
+vim.opt.redrawtime      = 10000
 -- 语言
-vim.o.langmenu       = "en_US.UTF-8"
+vim.o.langmenu          = "en_US.UTF-8"
 -- 开启folding
-vim.wo.foldmethod    = "expr"
-vim.wo.foldexpr      = "nvim_treesitter#foldexpr()"
-vim.wo.foldlevel     = 99
-vim.o.foldcolumn     = '1'
-vim.o.foldlevelstart = 99
-vim.o.foldenable     = true
+vim.wo.foldmethod       = "expr"
+vim.wo.foldexpr         = "nvim_treesitter#foldexpr()"
+vim.wo.foldlevel        = 99
+vim.o.foldcolumn        = '1'
+vim.o.foldlevelstart    = 99
+vim.o.foldenable        = true
 -- 命令行高度
-vim.o.cmdheight      = 0
+vim.o.cmdheight         = 0
+-- 设置python位置
+vim.g.python3_host_prog = "/usr/bin/python"
