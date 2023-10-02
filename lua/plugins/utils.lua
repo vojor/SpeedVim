7d18efd397704a51f81aa18815471291a544687f
alounel <uhavous@gmail.com>
2023-10-02 23:21:25 +0800

    optimize

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -64,16 +64,15 @@ return {
     -- 显示缩进线
     {
         "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
         lazy = true,
         event = { "BufReadPost", "BufNewFile" },
         opts = {
-            -- 显示当前所在区域
-            show_current_context = true,
-            -- 显示当前所在区域的开始位置
-            show_current_context_start = true,
-            -- 显示行尾符
-            show_end_of_line = true,
-            space_char_blankline = " ",
+            indent = {
+                char = "│",
+                tab_char = "│",
+            },
+            scope = { enabled = false },
         }
     },
     -- 彩虹括号
