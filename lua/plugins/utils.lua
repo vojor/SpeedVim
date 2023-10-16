16581f8f469b150ae00fc7edfa243436e473d833
alounel <uhavous@gmail.com>
2023-10-16 18:37:41 +0800

    optimize

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -120,7 +120,7 @@ return {
         event = { "BufReadPost", "BufNewFile" },
         config = function()
             local rainbow_delimiters = require 'rainbow-delimiters'
-            require("rainbow-delimiters.setup")({
+            require("rainbow-delimiters.setup").setup({
                 strategy = {
                     [''] = rainbow_delimiters.strategy['global'],
                 },
