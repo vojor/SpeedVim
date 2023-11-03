f3bba14827acd0916306b593234bd710322f79d0
alounel <uhavous@gmail.com>
2023-11-03 19:09:02 +0800

    optimize

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,54 +1,23 @@
-local kopts = { noremap = true, silent = true }
-
-local function mapkey(mode, lhs, rhs)
-    vim.api.nvim_set_keymap(mode, lhs, rhs, kopts)
-end
-
-local function mapcmd(key, cmd)
-    vim.api.nvim_set_keymap('n', key, ':' .. cmd .. '<cr>', kopts)
-end
-
--- C
 -- 快捷命令
-mapkey('n', '<leader>ce', ':e<space>')
-mapcmd('<leader>cw', 'w')
-mapcmd('<leader>cW', 'wa')
-mapcmd('<leader>cE', 'e!')
-mapcmd('<leader>cq', 'q')
-mapcmd('<leader>cQ', 'q!')
-mapcmd('<leader>cx', 'x')
+vim.keymap.set('n', '<leader>ce', ':e<space>')
+vim.keymap.set("n", '<leader>cw', '<cmd>w<CR>')
+vim.keymap.set("n", '<leader>cW', '<cmd>wa<CR>')
+vim.keymap.set("n", '<leader>cE', '<cmd>e!<CR>')
+vim.keymap.set("n", '<leader>cq', '<cmd>q<CR>')
+vim.keymap.set("n", '<leader>cQ', '<cmd>q!<CR>')
+vim.keymap.set("n", '<leader>cx', '<cmd>x<CR>')
 -- 健康检查
-mapcmd("<leader>ch", "checkhealth")
--- lazy
-mapcmd("<leader>lz", "Lazy")
-
--- search
-mapkey('n', '<leader>sw', '/\\<lt>\\><left><left>')
+vim.keymap.set("n","<leader>ch", "<cmd>checkhealth<CR>")
 
 
--- T
--- ToggleTerm
 -- 退出终端插入模式
-mapkey("t", "<Esc>", "<C-\\><C-n>")
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 -- window
-mapkey('n', '<leader>wh', '<c-w>h')
-mapkey('n', '<leader>wj', '<c-w>j')
-mapkey('n', '<leader>wk', '<c-w>k')
-mapkey('n', '<leader>wl', '<c-w>l')
-mapkey('n', '<leader>w1', '<c-w>o')
-mapkey("n", "<leader>wv", "<c-w>v") -- 水平新增窗口
-mapkey("n", "<leader>ws", "<c-w>s") -- 垂直新增窗口
-mapcmd('<leader>wx', 'x')
-mapcmd('<leader>w2', 'sp')
-mapcmd('<leader>w3', 'vs')
--- window resize
-mapkey('n', '<m-9>', '<c-w><')
-mapkey('n', '<m-0>', '<c-w>>')
-mapkey('n', '<m-->', '<c-w>-')
-mapkey('n', '<m-=>', '<c-w>+')
-mapkey('n', '<m-r>', 'resize<space>')
-mapkey('n', '<m-t>', 'vertical resize<space>')
---单行或多行移动
-mapkey("v", "J", ":m '>+1<CR>gv=gv")
-mapkey("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set('n', '<leader>wh', '<c-w>h')
+vim.keymap.set('n', '<leader>wj', '<c-w>j')
+vim.keymap.set('n', '<leader>wk', '<c-w>k')
+vim.keymap.set('n', '<leader>wl', '<c-w>l')
+vim.keymap.set('n', '<leader>w1', '<c-w>o')
+vim.keymap.set("n", "<leader>wv", "<c-w>v") -- 水平新增窗口
+vim.keymap.set("n", "<leader>ws", "<c-w>s") -- 垂直新增窗口
--- lua/plugins/textcope.lua
+++ lua/plugins/textcope.lua
@@ -18,67 +18,14 @@ return {
     },
     -- 文本字符替换
     {
-        "nvim-pack/nvim-spectre",
-        cmd = "Spectre",
+        "AckslD/muren.nvim",
         lazy = true,
         keys = {
-            { "<leader>ro", mode = "n", desc = "Toggle Spectre" },
-            { "<leader>rh", mode = "n", desc = "Search Current Word" },
-            { "<leader>rh", mode = "v", desc = "Search Current Word" },
-            { "<leader>rf", mode = "n", desc = "Search On Current File" },
+            { "<leader>rg", "<cmd>MurenToggle<CR>", desc = "Toggle Muren UI" },
+            { "<leader>rf", "<cmd>MurenFresh<CR>",  desc = "Open Muren UI Fresh" },
+            { "<leader>ru", "<cmd>MurenUnique<CR>", desc = "Unique Matches" },
         },
-        config = function()
-            require("spectre").setup({
-                mapping = {
-                    ['toggle_line'] = {
-                        map = "<leader>rd",
-                    },
-                    ['enter_file'] = {
-                        map = "<CR>",
-                    },
-                    ["send_to_qf"] = {
-                        map = "<leader>rq",
-                    },
-                    ['replace_cmd'] = {
-                        map = "<leader>rn",
-                    },
-                    ['show_option_menu'] = {
-                        map = "<leader>rm",
-                    },
-                    ['run_current_replace'] = {
-                        map = "<leader>rc",
-                    },
-                    ['run_replace'] = {
-                        map = "<leader>ra",
-                    },
-                    ['change_view_mode'] = {
-                        map = "<leader>rv",
-                    },
-                    ['change_replace_sed'] = {
-                        map = "<leader>rs",
-                    },
-                    ['change_replace_oxi'] = {
-                        map = "<leader>rx",
-                    },
-                    ['toggle_live_update'] = {
-                        map = "<leader>ru",
-                    },
-                    ['toggle_ignore_case'] = {
-                        map = "<leader>ri",
-                    },
-                    ['toggle_ignore_hidden'] = {
-                        map = "<leader>re",
-                    },
-                    ['resume_last_search'] = {
-                        map = "<leader>rb",
-                    },
-                }
-            })
-            vim.keymap.set('n', '<leader>ro', '<cmd>lua require("spectre").toggle()<CR>')
-            vim.keymap.set('n', '<leader>rh', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>')
-            vim.keymap.set('v', '<leader>rh', '<esc><cmd>lua require("spectre").open_visual()<CR>')
-            vim.keymap.set('n', '<leader>rf', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>')
-        end
+        opts = {},
     },
     -- 修改定界符
     {
