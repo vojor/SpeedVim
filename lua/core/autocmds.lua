d8da14638f8845cbe63a1e9877beb68d5b963ae5
alounel <uhavous@gmail.com>
2023-11-30 12:14:26 +0800

    optimize

---
+++ lua/core/autocmds.lua
@@ -0,0 +1,35 @@
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+    callback = function()
+        local current_tab = vim.fn.tabpagenr()
+        vim.cmd("tabdo wincmd =")
+        vim.cmd("tabnext " .. current_tab)
+    end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = {
+        "PlenaryTestPopup",
+        "help",
+        "lspinfo",
+        "man",
+        "notify",
+        "qf",
+        "query",
+        "checkhealth",
+        "DiffviewFiles",
+    },
+    callback = function(event)
+        vim.bo[event.buf].buflisted = false
+        vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    callback = function(event)
+        if event.match:match("^%w%w+://") then
+            return
+        end
+        local file = vim.loop.fs_realpath(event.match) or event.match
+        vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+    end,
+})
