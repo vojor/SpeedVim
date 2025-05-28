0112bf28c8856e1decdbb4f0b80e171383df881b
Smith Alike <sojenir@gmail.com>
2025-05-29 00:38:47 +0800

    update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,35 +1,23 @@
 return {
-    -- 语法高亮
     {
         "nvim-treesitter/nvim-treesitter",
-        build = { ":TSUpdate" },
-        lazy = true,
-        event = { "BufReadPost", "BufNewFile" },
-        cmd = { "TSUpdate", "TSUpdateSync" },
-        opts = {
-            sync_install = false,
-            ensure_installed = { "c", "cpp", "lua", "vim", "cmake", "bash", "markdown", "markdown_inline",
-                "regex", "yaml" },
-            ignore_install = {},
-            auto_install = false,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false
-            },
-            incremental_selection = {
-                enable = false,
-            },
-            indent = {
-                enable = true
-            },
-        },
-        config = function(_, opts)
-            require("nvim-treesitter.configs").setup(opts)
-            vim.opt.foldmethod = "expr"
-            vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+        branch = "main",
+        lazy = false,
+        build = ":TSUpdate",
+        config = function()
+            local ensure_installed = { "bash", "c", "cpp", "cmake", "lua", "markdown", "markdown_inline", "regex", "vim",
+                "yaml" }
+            require("nvim-treesitter").install(ensure_installed):wait(300000)
+            vim.api.nvim_create_autocmd('FileType', {
+                pattern = ensure_installed,
+                callback = function()
+                    vim.treesitter.start()
+                    vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end,
+            })
         end
     },
-    -- 屏幕显示函数前几行
     {
         "nvim-treesitter/nvim-treesitter-context",
         lazy = true,
